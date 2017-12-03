272a23d2d51050701518bcc16d8ce9ee8a7246dc
Sim Gill <simgill89@gmail.com>
2017-12-03 14:14:21 -0800

    updates

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from "../data.service";
 import { Coffee } from "../logic/Coffee";
 import { Router } from "@angular/router";
+import { GeolocationService } from "../geolocation.service";
 
 @Component({
   selector: 'app-list',
@@ -13,10 +14,30 @@ export class ListComponent implements OnInit {
   list : [Coffee]
 
   constructor(private data: DataService,
-              private router: Router) { }
+              private router: Router,
+              private geolocation: GeolocationService) { }
 
   goDetails(coffee: Coffee) {
     this.router.navigate(["/coffee", coffee._id]);
+  }
+
+  goMap(coffee: Coffee) {
+    const mapURL = this.geolocation.getMapLink(coffee.location);
+    location.href = mapURL;
+  }
+
+  share(coffee: Coffee) {
+    const shareText = `I had this coffee at ${coffee.place} and for me it's a ${coffee.rating} star coffee`;
+    if ('share' in navigator) {
+      (navigator as any).share({
+        title: coffee.name,
+        text: shareText,
+        url: window.location.href
+      }).then( () => console.log("shared")).catch( () => console.log("error sharing"));
+    } else {
+      const shareURL = `whatsapp://send?text=${encodeURIComponent(shareText)}`;
+      location.href = shareURL;
+    }
   }
 
   ngOnInit() {
